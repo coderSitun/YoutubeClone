f24c42f1c68305da71e13c94e524aec624d1b7bb
coderSitun <ksaswat007@gmail.com>
2022-12-06 18:10:44 +0530

    Added overrides for onCreateOptionsMenu & onOptionsItemSelected

--- app/src/main/java/com/example/youtubeclone/MainActivity.java
+++ app/src/main/java/com/example/youtubeclone/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.youtubeclone;
 
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.FrameLayout;
 import android.widget.Toast;
@@ -29,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
 
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        getSupportActionBar().setTitle("");
 
         bottomNavigationView = findViewById(R.id.bottom_navigation);
         frameLayout = findViewById(R.id.frame_layout);
@@ -69,5 +72,30 @@ public class MainActivity extends AppCompatActivity {
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
         fragmentTransaction.replace(R.id.frame_layout, fragment);
         fragmentTransaction.commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId())
+        {
+            case R.id.notification:
+                Toast.makeText(this, "Notification", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.search:
+                Toast.makeText(this, "Search", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.account:
+                Toast.makeText(this, "Account", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
